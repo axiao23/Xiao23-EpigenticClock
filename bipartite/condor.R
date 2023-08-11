28265df05617178107592f5c024cd0ac90f45f87
axiao23 <augexiao6788@gmail.com>
2023-08-11 13:49:29 -0400

    Update condor.R

--- bipartite/condor.R
+++ bipartite/condor.R
@@ -6,26 +6,13 @@ devtools::install_github("jplatig/condor")
 library(condor)
 elist = read.csv("../data/edgelist_TCGA_LUAD_data.csv")
 
-
-
 condor.object <- createCondorObject(elist)
 names(condor.object)
 condor.object <- condorCluster(condor.object)
 print(condor.object$red.memb)
 print(condor.object$blue.memb)
 condor.object <- condorQscore(condor.object)
-condorPlotHeatmap(condor.object)
-mod <- sort(condor.object$modularity, decreasing=T)
-barplot(mod, horiz=T, main = 'Modularity',  xlab = 'Modularity',cex.names=0.4, xlim = c(0,0.2), col='orchid')
-condorPlotCommunities(
-  condor.object,
-  color_list=c("darkgreen","darkorange", "darkviolet"),
-  point.size = .02,
-  xlab = "Methylation",
-  ylab = "Gene"
-)
-q_in <- condor.object$qscores$red.qscore
-out <- condorCoreEnrich(c("TUBB2B","SCGN"),q=q_in,perm=TRUE,plot.hist=TRUE)  
+
 
 
 
@@ -39,16 +26,30 @@ V(g)$color <- V(g)$type
 V(g)$color=gsub("FALSE","red",V(g)$color)
 V(g)$color=gsub("TRUE","blue",V(g)$color)
 plot(g, edge.color="gray30",edge.width=E(g)$weight, layout=layout_as_bipartite)
-#condor analysis
+
+#CONDOR ANALYSIS
+#enrich
 q_methylation <- condor.object$qscores$red.qscore
 core_stats <- condorCoreEnrich(test_nodes=c("cg23180365","cg00702638"),
                                q=q_methylation,perm=TRUE,plot.hist=TRUE)
 
 q_genes <- condor.object$qscores$blue.qscore
-core_stat_1 <- condorCoreEnrich(test_nodes=c("TUBB2B"),
+core_stat_1 <- condorCoreEnrich(test_nodes=c("TUBB2B", "BAZ2A"),
                                 q=q_genes,perm=TRUE,plot.hist=TRUE)
 
-gtoy = graph.edgelist(as.matrix(elist[1:2]), directed=FALSE)
-plot(gtoy,vertex.label.dist=2)
 
+#heatmap
 condorPlotHeatmap(condor.object)
+
+#barplot
+mod <- sort(condor.object$modularity, decreasing=T)
+barplot(mod, horiz=T, main = 'Modularity',  xlab = 'Modularity',cex.names=0.4, xlim = c(0,0.15), col='orchid')
+
+#communities
+condorPlotCommunities(
+  condor.object,
+  color_list=c("darkgreen","darkorange", "darkviolet"),
+  point.size = .01,
+  xlab = "Methylation",
+  ylab = "Gene"
+)
